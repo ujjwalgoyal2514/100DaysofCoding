fda317612368b8156ca406d00292d5f9f811534f
Ujjwal Goyal <114306876+ujjwalgoyal2514@users.noreply.github.com>
2023-05-23 22:51:07 +0530

    Add files via upload

---
+++ Intersection_of_TwoArrays.py
@@ -0,0 +1,12 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+            res = []
+            map = {}
+            for i in nums1:
+                map[i] = map[i]+1 if i in map else 1
+            for j in nums2:
+                if j in map and map[j] > 0:
+                    res.append(j)
+                    map[j] = 0
+
+            return res
