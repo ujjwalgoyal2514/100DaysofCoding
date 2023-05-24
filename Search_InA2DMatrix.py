5d7471bd79589c95f8c8c008d0c42eaabebd9cb4
Ujjwal Goyal <114306876+ujjwalgoyal2514@users.noreply.github.com>
2023-05-24 22:43:23 +0530

    Add files via upload

---
+++ Search_InA2DMatrix.py
@@ -0,0 +1,20 @@
+class Solution:
+    def searchMatrix(self, mat: List[List[int]], target: int) -> bool:
+        m=len(mat)
+        n=len(mat[0])
+        
+        for i in range(m):
+            if mat[i][0]<=target and mat[i][-1]>=target:
+                lo=0
+                hi=n
+                while (lo<hi):
+                    mid=(lo+hi)//2
+                    
+                    if mat[i][mid]==target:
+                        return True
+                    elif mat[i][mid]<target:
+                        lo = mid + 1
+                    else:
+                        hi = mid
+                        
+        return False
